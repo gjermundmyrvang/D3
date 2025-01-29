fd33be7ff94fab8dc009b9cec266aebf0cec04c4
gjermundmyrvang <myrvang.gjermund@gmail.com>
2025-01-29 22:34:44 +0100

    add a scale for x and y axis

--- Exercise1/script.js
+++ Exercise1/script.js
@@ -12,3 +12,10 @@ const svg = d3
   .attr("height", height + margin.top + margin.bottom)
   .append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+const MAX_X = 100;
+const MAX_Y = 100;
+const MAX_R = 200;
+
+const x = d3.scaleLinear().domain([0, MAX_X]).range([0, width]);
+const y = d3.scaleLinear().domain([0, MAX_Y]).range([height, 0]);
