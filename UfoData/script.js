bac95985953d2e583720f716dd2f995fc71888f5
gjermundmyrvang <myrvang.gjermund@gmail.com>
2025-02-02 23:38:42 +0100

    add d3 tree structure to the data

--- UfoData/script.js
+++ UfoData/script.js
@@ -1,14 +1,3 @@
-const margin = { top: 60, right: 30, bottom: 60, left: 70 },
-  width = 800 - margin.left - margin.right,
-  height = 600 - margin.top - margin.bottom;
-
-const svg = d3
-  .select("body")
-  .append("svg")
-  .attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom)
-  .attr("transform", `translate(${margin.left}, ${margin.top})`);
-
 const formatTime = d3.utcFormat("%B %d, %Y");
 
 d3.csv("testdata.csv").then((data) => {
@@ -30,7 +19,76 @@ d3.csv("testdata.csv").then((data) => {
     children: buildHierarchy(parsedData),
   };
   console.log(hierarchy);
+
+  const width = 1605;
+
+  const root = d3.hierarchy(hierarchy);
+
+  const dx = 10;
+  const dy = width / (root.height + 1);
+
+  const tree = d3.tree().nodeSize([dx, dy]);
+
+  tree(root);
+
+  let x0 = Infinity;
+  let x1 = -x0;
+  root.each((d) => {
+    if (d.x > x1) x1 = d.x;
+    if (d.x < x0) x0 = d.x;
+  });
+
+  const height = x1 - x0 + dx * 2;
+
+  const svg = d3
+    .select("body")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .attr("viewBox", [-dy / 3, x0 - dx, width, height])
+    .attr("style", "max-width: 100%; height: auto; font: 10px sans-serif;");
+
+  const link = svg
+    .append("g")
+    .attr("fill", "none")
+    .attr("stroke", "#555")
+    .attr("stroke-opacity", 0.4)
+    .attr("stroke-width", 1.5)
+    .selectAll()
+    .data(root.links())
+    .join("path")
+    .attr(
+      "d",
+      d3
+        .linkHorizontal()
+        .x((d) => d.y)
+        .y((d) => d.x)
+    );
+
+  const node = svg
+    .append("g")
+    .attr("stroke-linejoin", "round")
+    .attr("stroke-width", 3)
+    .selectAll()
+    .data(root.descendants())
+    .join("g")
+    .attr("transform", (d) => `translate(${d.y},${d.x})`);
+
+  node
+    .append("circle")
+    .attr("fill", (d) => (d.children ? "#555" : "#999"))
+    .attr("r", 2.5);
+
+  node
+    .append("text")
+    .attr("dy", "0.31em")
+    .attr("x", (d) => (d.children ? -6 : 6))
+    .attr("text-anchor", (d) => (d.children ? "end" : "start"))
+    .text((d) => d.data.name)
+    .attr("stroke", "white")
+    .attr("paint-order", "stroke");
 });
+
 function buildHierarchy(data) {
   const root = {};
 
