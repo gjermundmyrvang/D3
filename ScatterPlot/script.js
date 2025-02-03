8eefc8b53f92b6af420cbce96e2409e13622729c
gjermundmyrvang <myrvang.gjermund@gmail.com>
2025-02-03 17:31:39 +0100

    make scatterplot responsive

--- ScatterPlot/script.js
+++ ScatterPlot/script.js
@@ -1,14 +1,14 @@
-const margin = { top: 50, right: 30, bottom: 40, left: 70 },
-  width = 800 - margin.left - margin.right,
-  height = 600 - margin.top - margin.bottom;
+const width = 800;
+const height = 600;
+const margin = 50;
 
 const svg = d3
-  .select("body")
+  .select("#canvas")
   .append("svg")
-  .attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom)
+  .attr("width", width + margin * 2)
+  .attr("height", height + margin * 2)
   .append("g")
-  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  .attr("transform", `translate(${margin}, ${margin})`);
 
 d3.csv("coffee_energy.csv").then((data) => {
   data.forEach((d) => {
@@ -21,13 +21,14 @@ d3.csv("coffee_energy.csv").then((data) => {
     .append("text")
     .attr("id", "title")
     .attr("x", width / 2)
-    .attr("y", 0 - margin.top / 2)
+    .attr("y", -20)
     .attr("text-anchor", "middle")
     .style("font-size", "20px")
     .style("text-decoration", "bold")
     .text("Coffee vs Energy Scatter Plots");
 
   const maxX = Math.max(...data.map((d) => d.coffeeAmount));
+
   const xAxis = d3.scaleLinear().domain([0, maxX]).range([0, width]);
 
   svg
@@ -40,6 +41,26 @@ d3.csv("coffee_energy.csv").then((data) => {
   svg.append("g").call(d3.axisLeft(yAxis));
 
   svg
+    .append("text")
+    .attr("id", "title")
+    .attr("x", width / 2)
+    .attr("y", height + 40)
+    .attr("text-anchor", "middle")
+    .style("font-size", "20px")
+    .style("text-decoration", "bold")
+    .text("Coffee amount (mg)");
+
+  svg
+    .append("text")
+    .attr("id", "title")
+    .attr("transform", "rotate(-90)")
+    .attr("x", -height / 2)
+    .attr("y", margin)
+    .style("font-size", "20px")
+    .style("text-decoration", "bold")
+    .text("Energy level");
+
+  svg
     .append("g")
     .selectAll("dot")
     .data(data)
@@ -50,25 +71,6 @@ d3.csv("coffee_energy.csv").then((data) => {
     .attr("cy", (d) => {
       return yAxis(d.energyLevel);
     })
-    .attr("r", 10.0)
+    .attr("r", 5)
     .style("fill", "#ff0000");
-
-  svg
-    .append("text")
-    .attr("id", "title")
-    .attr("x", width - margin.right * 2)
-    .attr("y", height - 20)
-    .attr("text-anchor", "middle")
-    .style("font-size", "20px")
-    .style("text-decoration", "bold")
-    .text("Coffee amount (mg)");
-
-  svg
-    .append("text")
-    .attr("id", "title")
-    .attr("x", 20)
-    .attr("y", height / 2)
-    .style("font-size", "20px")
-    .style("text-decoration", "bold")
-    .text("Energy level");
 });
