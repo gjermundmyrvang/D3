12e78b81751aab0bc63fab83f56cbe89f1769e1a
gjermundmyrvang <myrvang.gjermund@gmail.com>
2025-02-03 13:58:31 +0100

    style exercise to use full width and height

--- Exercise1/script.js
+++ Exercise1/script.js
@@ -1,17 +1,16 @@
-// Declaring some properties for the canvas
-const margin = { top: 60, right: 30, bottom: 60, left: 70 },
-  width = 1738 - margin.left - margin.right,
-  height = 850 - margin.top - margin.bottom;
+const width = window.innerWidth;
+const height = window.innerHeight;
 
 // Add <svg> inside body
 const svg = d3
-  .select("body")
+  .select("#canvas")
   .append("svg")
   // When using the width and height properties later this ensures some padding
-  .attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+  .attr("width", width)
+  .attr("height", height)
+  .attr("viewBox", `0 0 ${width} ${height}`)
+  .attr("preserveAspectRatio", "xMidYMid meet")
+  .append("g");
 
 const MAX_X = 100;
 const MAX_Y = 100;
